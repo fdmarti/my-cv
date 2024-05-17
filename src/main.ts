4c10014a9a21a8e07b6b58bb65d7b3e8b4677010
f-d.martinez <f-d.martinez@viseven.com>
2024-05-17 16:05:05 -0300

    update(*) analytics added

--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,8 @@ import App from './App.vue';
 import { i18n } from './locale/main';
 
 const app = createApp(App);
+import { inject } from '@vercel/analytics';
 
+const mode = import.meta.env.MODE;
+inject({ mode: mode ? 'development' : 'production' });
 app.use(i18n).mount('#app');
